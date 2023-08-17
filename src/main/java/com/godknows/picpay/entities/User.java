82a66e20e3f2d47a40f7d7d41c58892d40a31fb7
blicuadriDev <blicuadriDev@gmail.com>
2023-08-17 09:01:55 -0300

    Notification system set up

--- src/main/java/com/godknows/picpay/entities/User.java
+++ src/main/java/com/godknows/picpay/entities/User.java
@@ -38,13 +38,41 @@ public class User {
 	private BigDecimal balance;
 	@Enumerated(EnumType.STRING)
 	private UserType userType;
-	
-	
-	public UserType getUserType() {
-		return userType;
+	public long getId() {
+		return id;
 	}
-	public void setUserType(UserType userType) {
-		this.userType = userType;
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDocument() {
+		return document;
+	}
+	public void setDocument(String document) {
+		this.document = document;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	public BigDecimal getBalance() {
 		return balance;
@@ -52,10 +80,12 @@ public class User {
 	public void setBalance(BigDecimal balance) {
 		this.balance = balance;
 	}
-	
-	
-	
-	
-	
+	public UserType getUserType() {
+		return userType;
+	}
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+
 
 }
---
+++ src/main/java/com/godknows/picpay/services/NotificationService.java
@@ -0,0 +1,32 @@
+package com.godknows.picpay.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.godknows.picpay.dtos.NotificationDTO;
+import com.godknows.picpay.entities.User;
+
+@Service
+public class NotificationService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public void sendNotification(User user, String message) throws Exception {
+		String email = user.getEmail();
+		
+		NotificationDTO notificationRequest = new NotificationDTO(email, message);
+		
+		ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+		
+		if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+			System.out.println("Notification system not available");
+			throw new Exception("Notificataion service unavailable");
+		}
+		
+	}
+
+}
